49a6ba47e3713739c669d515c25bc7a0d285567b
stevenwhy <stevenyildirim@gmail.com>
2022-03-30 14:41:07 -0400

    storing stream of nums in heaps and calculating median

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,52 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val stream = NumberStream()
+    stream.insertNum(3)
+    stream.insertNum(1)
+    println("expected median=2 actual=${stream.findMedian()}")
+    stream.insertNum(5)
+    println("expected median=3 actual=${stream.findMedian()}")
+    stream.insertNum(4)
+    println("expected median=3.5 actual=${stream.findMedian()}")
+
+}
+
+class NumberStream() {
+    // max heap
+    private val firstHalf: PriorityQueue<Int> = PriorityQueue(compareByDescending { it })
+    // min heap
+    private val secondHalf: PriorityQueue<Int> = PriorityQueue()
+
+    // O(log(n)) insertion in priority queues
+    fun insertNum(num: Int) {
+        if(firstHalf.isEmpty()) firstHalf.add(num)
+        else if(secondHalf.isEmpty()) {
+            if(num < firstHalf.peek()) {
+                secondHalf.add(firstHalf.poll())
+                firstHalf.add(num)
+            }
+        } else if(num < firstHalf.peek()) {
+            // keep balance
+            if(firstHalf.size > secondHalf.size) secondHalf.add(firstHalf.poll())
+            firstHalf.add(num)
+        }
+        else if(num > secondHalf.peek()) {
+            // keep balance
+            if(secondHalf.size > firstHalf.size) firstHalf.add(secondHalf.poll())
+            secondHalf.add(num)
+        }
+        else {
+            if(secondHalf.size > firstHalf.size) firstHalf.add(num)
+            else secondHalf.add(num)
+        }
+        println("First half: $firstHalf")
+        println("Second half: $secondHalf")
+    }
+
+    fun findMedian(): Double {
+        if(secondHalf.size == firstHalf.size) return (secondHalf.peek() + firstHalf.peek()) / 2.0
+        else if(secondHalf.size > firstHalf.size) return secondHalf.peek().toDouble()
+        return firstHalf.peek().toDouble()
+    }
 }
